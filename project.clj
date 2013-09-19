4f5d80a5b7716be7bd6a294bf7385e9f74f87cd3
Michael Lopez <malatmals@gmail.com>
2013-09-19 14:00:16 -0600

    updating proj description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ariane "0.1.0-SNAPSHOT"
-  :description "Atom & RSS parser for Clojure"
-  :url "http://github.com/eprunier/ariane"
+  :description "Post RSS feeds to flipthis-webhook"
+  :url "https://github.com/xicubed/ariane"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
